9a069a9d470b95d3723402e98087aa2125bedc0c
Sacr1fice <sanunkurov@gmail.com>
2023-06-13 11:44:32 +0300

    dorabotka2

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -3,8 +3,8 @@ import static java.lang.Math.pow;
 public class CreditPaymentService {
     public double calculate(double percent, int time, int credit) {
         int payment;
-        double month_percent = percent / 1200;
-        double coefficient = month_percent * pow(1 + month_percent, time) / (pow(1 + month_percent, time) - 1);
+        double monthPercent = percent / 1200;
+        double coefficient = monthPercent * pow(1 + monthPercent, time) / (pow(1 + monthPercent, time) - 1);
         payment = (int) (credit * coefficient);
         return payment;
     }
